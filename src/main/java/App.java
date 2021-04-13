fc014c07ab749e2682adac6bbef8d807532134b7
sheila <devsheilawambui@gmail.com>
2021-04-13 23:27:30 +0300

    display user options on terminal

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,18 @@
+
+import java.io.*;
+
 public class App {
+    public static void main(String [] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((System.in)));
+        System.out.println("Welcome To Ceasar Cipher!");
+        boolean runningProgram = true;
+
+        while (runningProgram) {
+            System.out.println("**---------------------------------------------------------**");
+            System.out.println("Enter A, B or C to choose one of the following options");
+            System.out.println("A. Encrypt And Decrypt Message");
+            System.out.println("B. Encrypt And Decrypt File");
+            System.out.println("C. Exit");
+        }
+    }
 }
