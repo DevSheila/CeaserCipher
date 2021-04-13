694e7ca22c82fc4948ba3e74ee89cef172fa44ac
sheila <devsheilawambui@gmail.com>
2021-04-13 23:13:32 +0300

    add decrypting method

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -9,6 +9,25 @@ public class Decrypt {
 
     public String decryptMessage(String encryptedMessage, int shiftKey) {
 
-        return null;
+        this.encryptedMessage = encryptedMessage.toUpperCase();
+        this.shiftKey= shiftKey;
+
+        for(int i =0; i<this.encryptedMessage.length();i++){
+            if(!Character.isLetter(this.encryptedMessage.charAt(i))){
+                decryptedMessage.append(this.encryptedMessage.charAt(i));
+            }else{
+                currentCharPosition = alphabets.indexOf(this.encryptedMessage.charAt(i));
+                newCharPosition=(currentCharPosition - this.shiftKey )%26;
+
+                if(newCharPosition<0){
+                    newCharPosition = alphabets.length()+ newCharPosition;
+                }
+
+                newCharValue = alphabets.charAt((newCharPosition));
+                decryptedMessage.append(newCharValue);
+            }
+        }
+
+        return decryptedMessage.toString();
     }
 }
