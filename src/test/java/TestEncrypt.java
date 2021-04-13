87d03e32be218ab4354da9759bc5e27083a0e31f
sheila <devsheilawambui@gmail.com>
2021-04-13 23:02:28 +0300

    Test instantiation of all classes

--- src/test/java/TestEncrypt.java
+++ src/test/java/TestEncrypt.java
@@ -1,2 +1,29 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 public class TestEncrypt {
+    @Test
+    public void newCeasar_instantiatesCorrectly() {
+        Ceasar testCeasar = new Ceasar("we attack at dawn", 4);
+        assertEquals(true, testCeasar instanceof Ceasar);
+    }
+
+    @Test
+    public void newEncrypt_instantiatesCorrectly() {
+        EncryptClass testEncrypt = new EncryptClass();
+        assertEquals(true, testEncrypt instanceof EncryptClass);
+    }
+
+    @Test
+    public void newDecrypt_instantiatesCorrectly() {
+        Decrypt testDecrypt = new Decrypt();
+        assertEquals(true, testDecrypt instanceof Decrypt);
+    }
+
+    @Test
+    public void newApp_instantiatesCorrectly() {
+        App newApp = new App();
+        assertEquals(true, newApp instanceof App);
+    }
 }
